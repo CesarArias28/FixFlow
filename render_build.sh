d3f8b145c8ec49f18fd8893677c32e4fe3868e46
Julio Arias <cesararias2420@gmail.com>
2026-05-20 10:40:03 +0200

    Preparación del entorno

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
+# Build frontend
+cd frontend
 npm install
 npm run build
 
+# Install backend dependencies and migrate
+cd ../backend
 pipenv install
-
 pipenv run upgrade
