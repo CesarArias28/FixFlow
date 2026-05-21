6b6b38d0b4a52fa61e660cc9cb7ccfaab070bf57
Julio Arias <cesararias2420@gmail.com>
2026-05-21 15:55:38 +0200

    Table fix

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
-
-db = SQLAlchemy()
-
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from typing import Optional
 
@@ -12,21 +7,21 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(db.String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    role: Mapped[str] = mapped_column(db.String(20), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+    role: Mapped[str] = mapped_column(String(20), nullable=False)
 
 class Property(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    address: Mapped[str] = mapped_column(db.String(200), nullable=False)
+    address: Mapped[str] = mapped_column(String(200), nullable=False)
 
-    class Incidence(db.Model):
-     id: Mapped[int] = mapped_column(primary_key=True)
+class Incidence(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(db.String(100), nullable=False)
     description: Mapped[str] = mapped_column(db.String(500), nullable=False)
     status: Mapped[str] = mapped_column(db.String(20), default="Pendiente")
     severity: Mapped[Optional[str]] = mapped_column(db.String(20), nullable=True) 
     specialty: Mapped[Optional[str]] = mapped_column(db.String(50), nullable=True) 
       
-    tenant_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
-    property_id: Mapped[int] = mapped_column(db.ForeignKey('property.id'), nullable=False)
+    tenant_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    property_id: Mapped[int] = mapped_column(ForeignKey('property.id'), nullable=False)
